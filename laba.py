bafbe77f51ff167a59041deba7dc93de537d13c4
fibio <fibio.tany@gmail.com>
2017-12-14 18:31:41 +0200

    Added car10 to ds1

--- laba.py
+++ laba.py
@@ -60,7 +60,7 @@ car8 = Car(8, 'Audi', 45000, CarAttributes('Blue', 'Rain', 'Regular'))
 car9 = Car(9, 'Ford', 30000, CarAttributes('Violet', 'Sport', 'Level-1'))
 car10 = Car(10, 'Toyota', 32000, CarAttributes('Green', 'Show', 'Level-2'))
 
-dealership1 = Dealership([car1, car2, car3], 'KMac')
+dealership1 = Dealership([car1, car2, car3, car10], 'KMac')
 dealership2 = Dealership([car4, car5, car6, car7], 'JRM')
 dealership3 = Dealership([car8, car9], 'YPeng')
 
